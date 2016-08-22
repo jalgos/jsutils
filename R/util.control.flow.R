c338b85bcd9dcd658d6bbce07b0105a7657be59e
Sébastien Lamy de la Chapelle <sebastien@jalgos.com>
2016-08-22 15:37:57 +0200

    Added a logger option to print.call.stack

--- R/util.control.flow.R
+++ R/util.control.flow.R
@@ -8,5 +8,5 @@ print.call.stack <- function(...,
 {
     dump.frames()
     if(is.null(logger)) print(last.dump)
-    else jlog.error(logger, last.dump)
+    else jlogger::jlog.print(logger, last.dump)
 }

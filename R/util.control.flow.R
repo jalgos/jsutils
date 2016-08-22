586ab6933929ab4563ad255373e1c74fa7bd1283
Sébastien Lamy de la Chapelle <sebastien@jalgos.com>
2016-08-22 15:33:44 +0200

    Added a logger option to print.call.stack

--- R/util.control.flow.R
+++ R/util.control.flow.R
@@ -3,8 +3,10 @@
 #' Prints the entire call stack. Meant to be called upon an error occurring. Especially handy for the pbdR context where 'options(error = browser)' is unavailable.
 #' @param ... not used
 #' @export
-print.call.stack <- function(...)
+print.call.stack <- function(...,
+                             logger = NULL)
 {
     dump.frames()
-    print(last.dump)
+    if(is.null(logger)) print(last.dump)
+    else jlog.error(logger, last.dump)
 }

e545577fdd9accaeda567cfaedb2c7de7f60a024
Sébastien Lamy de la Chapelle <sebastien@jalgos.com>
2016-11-30 16:58:32 +0100

    Added function to create random.date

--- R/util.random.R
+++ R/util.random.R
@@ -12,3 +12,19 @@ random.word <- function(n,
     sapply(rbinom(n, size = wsize, prob = wprob),
            function(wl) paste(sample(letters, wl, replace = TRUE), collapse = ''))
 }
+
+#' Random Dates
+#'
+#' Generate random dates around a reference date
+#' @param n Sample size
+#' @param date.ref Reference date
+#' @param rdist distribution function to be used
+#' @param ... To be forwarded to `rdist`
+#' @export
+random.date <- function(n,
+                        date.ref = as.POSIXct(Sys.time()),
+                        rdist = function(..., sd = 10000000) rnorm(..., sd = sd),
+                        ...)
+{
+    date.ref + rdist(n, ...)
+}
